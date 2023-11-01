2a6881db17e1b897a000337f35329f2854f5338c
Shruti Gupta <shrutigupta5555@gmail.com>
2023-11-01 18:19:13 +0530

    remove hardcode string

--- src/hooks/useLiveValue2.js
+++ src/hooks/useLiveValue2.js
@@ -1,13 +1,15 @@
 import { useChannel } from "ably/react";
 import { useCallback, useState } from "react";
 
-import { getSpaceNameFromUrl } from "../utils/helper";
-
 export const useLiveValue = (componentName, self) => {
   const [value, setValue] = useState("");
 
+  const url = new URL(window.location.href);
+  const spaceNameInParams = url.searchParams.get("space");
+  
+
   /** 💡 Use rewind to get the last message from the channel. See https://ably.com/docs/channels/options/rewind 💡 */
-  const channelName = `[?rewind=1]probably-still-could-${componentName}`;
+  const channelName = `[?rewind=1]${spaceNameInParams}-${componentName}`;
   const { channel } = useChannel(channelName, (message) => {
     if (message.connectionId === self?.connectionId) return;
     setValue(message.data);
